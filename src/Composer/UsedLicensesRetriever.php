3cf77985d29ed5cd12ab22c85e6c82a770570636
Oliver Zieschang <146355503+exonn-ozies@users.noreply.github.com>
2024-03-12 23:01:52 +0100

    Refactoring type annotation

--- src/Composer/UsedLicensesRetriever.php
+++ src/Composer/UsedLicensesRetriever.php
@@ -30,8 +30,9 @@ class UsedLicensesRetriever
             throw new ProcessFailedException($process);
         }
 
-        /** @var array{dependencies:array<string,array{version:string,license:list<string>}>} */
-        self::$output = json_decode($process->getOutput(), true);
+        /** @var array{dependencies:array<string,array{version:string,license:list<string>}>} $output */
+        $output = json_decode($process->getOutput(), true);
+        self::$output = $output;
         return self::$output;
     }
 }
